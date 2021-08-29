1844270692603529b6bd906a03990657a82b9df8
Martin Guillon <martin.guillon@akylas.fr>
2021-08-29 16:19:46 +0200

    chore: dev

--- webapp/LocalHeightProvider.ts
+++ webapp/LocalHeightProvider.ts
@@ -5,7 +5,7 @@ import {MVTLoader} from '@loaders.gl/mvt';
 
 
 // const locahostServer = 'dev.tileserver.local'
-export const locahostServer = '192.168.1.51:8080'
+export const locahostServer = '0.0.0.0:8080'
 export class LocalHeightProvider extends MapProvider 
 {
 	public local: boolean
